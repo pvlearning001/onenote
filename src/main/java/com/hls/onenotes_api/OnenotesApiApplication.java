aa9cbe9b258a998a824bccb9601b509eb04f4623
Vu Minh Phuong <vu.minhphuong@petronas.com>
2024-04-29 12:35:05 +0800

    Init one notes api

---
+++ src/main/java/com/hls/onenotes_api/OnenotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.hls.onenotes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnenotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnenotesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hls/onenotes_api/OnenotesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hls.onenotes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnenotesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
